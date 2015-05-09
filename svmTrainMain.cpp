3468caeacbfa2d0624eb650729b37ac85b543f80
Aditya Gabbita <gk.aditya@gmail.com>
2015-05-09 12:06:45 -0400

    added write model to file functionality in svmTrainMain

--- svmTrainMain.cpp
+++ svmTrainMain.cpp
@@ -13,6 +13,7 @@
 #include "CycleTimer.h"
 #include <unistd.h>
 #include <mpi.h>
+#include <fstream>
 
 state_model state;
 
@@ -136,6 +137,7 @@ static void parse_arguments(int argc, char* argv[]) {
 
 void initialize_shard_sizes(int *shard_size, int cluster_size);
 void initialize_shard_disp(int *shard_size, int *shard_disp, int cluster_size);
+void write_out_model(float* x, int* y, float* alpha, float b);
 
 int main(int argc, char *argv[]) {
 
@@ -353,10 +355,17 @@ int main(int argc, char *argv[]) {
 		svm.destroy_t_cuda_handles();
 	}
 
-	//write model to file
-	//write_out_model(x, y, alpha, b);
+	//write training model
+	if(rank == 0) {
+		//refernce vectors as arrays
+		float *x_arr = &raw_x[0];
+		int *y_arr = &raw_y[0];
 
-	//cout << "Training model has been saved to the file " << state.model_file_name << "\n";
+		//write model to file
+		write_out_model(x_arr, y_arr, alpha, svm.b);
+
+		cout << "Training model has been saved to the file " << state.model_file_name << "\n";
+	}
 
 	//clear training data
 	//for(int i = 0 ; i < state.num_train_data; i++) {	
@@ -390,3 +399,34 @@ void initialize_shard_disp(int *shard_size, int *shard_disp, int cluster_size) {
 	}
 }
 
+void write_out_model(float* x, int* y, float* alpha, float b) {
+	//open output filestream for writing the model
+	ofstream model_file;
+	model_file.open(state.model_file_name);
+
+	if(model_file.is_open()) {
+		//gamma used in kernel for training
+		model_file << state.gamma << "\n";
+		model_file << b << "\n";
+
+		for(int i=0; i<state.num_train_data; i++) {
+			if(alpha[i] != 0) {
+				model_file << alpha[i] << "," << y[i];
+
+				//index for x
+				int idx = i*(state.num_attributes);
+
+				for(int j=0; j<state.num_attributes; j++) {
+					model_file << "," << x[idx + j];
+				}
+
+				model_file << "\n";
+			}
+		}
+
+		model_file.close();
+	} else {
+		cout << "Model output file " << state.model_file_name << " could not be opened for writing.\n";
+		exit(-1);
+	}
+}
